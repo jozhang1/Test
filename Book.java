1075a3df826bca2c61fa0b34424f40b31b7045f6
jozhang1 <jozhang9@hotmail.com>
2014-03-21 22:43:31 -0500

    change

---
+++ Book.java
@@ -0,0 +1,29 @@
+public class Book{
+	private String name;
+	private Test[] author;
+	private int qtyInStock;
+	public Book(String name, Test[] author, int qtyInstock){
+		this.name=name;
+		this.author = author;
+		this.qtyInStock  = qtyInstock;
+	}
+	public String getAuthor(){
+		String s = new String();
+		for(int i = 0;i<author.length;i++)
+			s+=( (author[i]).getName());
+		return s;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+	@Override
+	public boolean equals(Object book){
+		
+		if(book==null)return false;
+		if(this==book)return true;
+		Book b = (Book)book;
+		if(b.author==author && b.name==name && b.qtyInStock==qtyInStock)
+			return true;
+		return false;
+	}
+}
