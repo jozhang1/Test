16b1ea99f83286570c4655cb27654d6be558ecde
dmc-sw-web1 <jozhang9@gmail.com>
2014-04-16 09:59:50 -0500

    add func

--- tree.cpp
+++ tree.cpp
@@ -5,5 +5,18 @@ struct node{
 }
 
 void convert(node *root, node **head){
+    if(root==NULL)return;
+    static node *prev=NULL;
+    convert(root->left,head);
+    if(prev==NULL) *head=root;
+    else{
+        prev.right=root;
+        root.left=prev;
+    }
+    prev=root;
+    convert(root->right,head);
+}
+
+node* convert2(node *root){
     
 }
